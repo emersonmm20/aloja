e0e6271980225acedd9a9a8d0b7c4e77ece300e4
juliana <julianitha033@gmail.com>
2025-06-26 23:21:02 -0500

    Resueltos conflictos de merge en index.php y database.sql

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@ function conectarDB(){
 
     $host= "localhost";
     $user= "root";
-    $password="12345678";
+    $password="";
     $database="mydb";
 
     $mysqli = new mysqli($host, $user, $password, $database);
